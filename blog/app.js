a05a1e162125c7d76a2cc03817c46bba89cef91d
Jian <dreamhufly@163.com>
2016-05-14 10:38:20 +0800

    一个node博客

---
+++ blog/app.js
@@ -0,0 +1,107 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+// var flash = require('express-flash');
+
+var routes = require('./routes/index');
+var setting = require('./setting');
+var users = require('./routes/users');
+
+var session = require('express-session');
+var connect = require('connect');
+var MongoStore = require('connect-mongo')(session);
+
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser());
+app.use(session({
+  secret: setting.cookieSecret,
+  key: setting.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    url: 'mongodb://localhost/blog'
+    // url: 'mongodb://localhost/blog',
+    // db: setting.db,
+    // host: setting.host,
+    // port: setting.port
+  })
+}));
+// app.use(session({
+//   secret: setting.cookieSecret,
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { secure: true },
+//     store: new MongoStore({
+//     url: 'mongodb://localhost/blog',
+//     db: setting.db,
+//     host: setting.host,
+//     port: setting.port
+//   })
+// }))
+
+// app.use('/', routes);
+// app.use('/users', users);
+
+// 设置路由规则
+app.get('/', routes.index);
+app.get('/u/:user', routes.user);
+app.post('/post', routes.post);
+app.get('/reg', routes.reg);
+app.post('/reg', routes.doReg);
+app.get('/login', routes.login);
+app.post('/login', routes.doLogin);
+app.get('/logout', routes.logout);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(3000, function () {
+    console.log('app.js running at port 3000');
+});
+
+
+module.exports = app;
---
+++ blog/routes/index.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+var crypto = require('crypto');
+var setting = require('../setting');
+
+var User = require('../models/user.js');
+
+/* GET home page. */
+exports.index = function (req, res) {
+    res.render('index', {title: 'my blog'});
+}
+
+exports.user = function (req, res) {
+    res.render('user', {title: 'my blog'});
+}
+
+exports.post = function (req, res) {
+    res.render('post', {title: 'my blog'});
+}
+
+exports.reg = function (req, res) {
+    res.render('reg', {title: '注册页面'});
+}
+
+exports.doReg = function (req, res) {
+    if (req.body['password-repeat'] != req.body['password']) {
+        // console.log('11');
+        // req.session.error = '密码输入不一致，请检查后重试！';
+        return res.redirect('/login');
+    }
+    
+    // 对密码进行加密
+    var md5 = crypto.createHash('md5');
+    var password = md5.update(req.body.password).digest('base64');
+    
+    var newUser = new User({
+        name: req.body.username,
+        password: password
+    });
+    
+    User.find(newUser.name, function (err, user) {
+        if (user) {
+            // req.session.error = '该用户已存在';
+            return res.redirect('/reg');
+        }
+        else {
+            newUser.save(function (err) {
+                if (err) {
+                    // req.session.err = err;
+                    return res.redirect('/reg');
+                }
+                req.session.user = newUser;
+                // req.session.success = '注册成功';
+                // res.locals.user = newUser;
+                res.redirect('/');
+            })
+        }
+    })
+    // res.render('doReg', {title: 'my blog'});
+}
+
+exports.login = function (req, res) {
+    res.render('login', {title: '登录页面'});
+}
+
+exports.doLogin = function (req, res) {
+    res.render('doLogin', {title: 'my blog'});
+}
+
+exports.logout = function (req, res) {
+    res.logout('index', {title: 'my blog'});
+}
+
+
+
